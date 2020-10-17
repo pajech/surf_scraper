e46cbd29486097a07ed513e454b232d1b3d7081b
pajech <pajechy@gmail.com>
2020-10-17 11:03:41 +1100

    removing testing code

--- general/exporter.py
+++ general/exporter.py
@@ -12,10 +12,5 @@ if not os.path.isdir(application_folder + '/'+local_folder_table_dumps): os.make
 
 def export_local( dataframe, beach, surf_or_snow = 'Surf' ):
     dataframe_dict = dataframe.dtypes.to_frame('dtypes').reset_index()
-    print(application_folder + '/' + local_folder_table_dumps)
     dataframe_dict.to_csv(application_folder + '/surf_scraper/' + local_folder_table_dumps + '/' + beach+'Dict.csv', index = False)
     dataframe.to_csv(application_folder + '/surf_scraper/' + local_folder_table_dumps +'/' + beach+'{surf_snow}Report.csv'.format(surf_snow = surf_or_snow), index = False)
-    print(dataframe)
-    print(application_folder)
-    print(local_folder_table_dumps)
-    print('\n'*10)
